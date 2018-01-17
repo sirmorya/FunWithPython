41507ba4da64ac9e07bea8d2b93bea77128ba1ba
Ankit Sirmorya <sirmorya3ank@gmail.com>
2018-01-17 07:55:22 -0800

    BinaryTree from inorder and pre-order

---
+++ src/arrays/BinaryTreeFromPreAndInorder.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        if len(preorder) == 0:
+            return None
+        if len(preorder) == 1:
+            return TreeNode(preorder[0])
+        root = TreeNode(preorder[0])
+        index = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1 : index + 1], inorder[0 : index])
+        root.right = self.buildTree(preorder[index + 1 : len(preorder)], inorder[index + 1 : len(inorder)])
+        return root
